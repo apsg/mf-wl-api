60e7414eff54789d0f867c8fb33ec51ce352583c
Szymon Gackowski <szymon.gackowski@gmail.com>
2022-02-20 12:30:54 +0100

    2022-02-20_12-30-54

--- tests/SearchBankAccountTest.php
+++ tests/SearchBankAccountTest.php
@@ -6,14 +6,6 @@ class SearchBankAccountTest extends TestCase
     /** @test */
     public function it_finds_valid_account()
     {
-        // given
-        $number = '32116022020000000247781296';
-
-        // when
-        $response = $this->mf->searchBankAccount($number);
-
-        // then
-
-        dd($response);
+        $this->assertTrue(true); // TODO
     }
 }
